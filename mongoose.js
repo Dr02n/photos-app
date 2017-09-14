9e2b2358d8f6549b56ccca1903038b496354b360
s.butenko <serj_pearl@mail.ru>
2017-09-14 10:34:30 +0300

    code cleanup

--- mongoose.js
+++ mongoose.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
 
 mongoose.Promise = Promise;
-
 mongoose.plugin(beautifyUnique);
-
 mongoose.set('debug', process.env.NODE_ENV === 'development');
-
 mongoose.connect(process.env.MONGO_URI, { useMongoClient: true })
-  .then(() => null, err => console.error(err.message));
+  .then(
+    () => null,
+    err => console.error(err.message)
+  );
