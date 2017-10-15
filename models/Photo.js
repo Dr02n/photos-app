f0bab840ada38e5fc75f886511091eb29b34eb87
Sergei <you@example.com>
2017-10-15 23:39:58 +0300

    photos thums

--- models/Photo.js
+++ models/Photo.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
-const fs = require('fs-extra');
-const Jimp = require('jimp');
-const User = require('./User');
+const mongoose = require('mongoose')
+const fs = require('fs-extra')
+const Jimp = require('jimp')
+const User = require('./User')
 
 
 const Photo = new mongoose.Schema({
   name: {
     type: String,
-    default: 'No name'
+    default: 'No name' // TODO: {album.name} photo {number}
   },
   description: {
     type: String,
-    default: 'Lorem ipsum'
+    default: ''
   },
   album: {
     type: mongoose.Schema.Types.ObjectId,
@@ -25,7 +25,7 @@ const Photo = new mongoose.Schema({
   }
 }, {
   timestamps: true
-});
+})
 
 /**
  * VIRTUALS
@@ -35,66 +35,72 @@ Photo.virtual('comments', {
   ref: 'Comment',
   localField: '_id',
   foreignField: 'photo'
-});
+})
 
 Photo.virtual('filename').get(function () {
-  return `${this.id}.${this.extension}`;
-});
+  return `${this.id}.${this.extension}`
+})
 
-Photo.virtual('path').get(function () {
-  return 'public/uploads/photos/' + this.filename;
-});
+// Photo.virtual('path').get(function () {
+//   return 'public/uploads/photos/' + this.filename
+// })
 
 Photo.virtual('url').get(function () {
-  return '/uploads/photos/' + this.filename;
-});
+  return '/uploads/photos/original/' + this.filename
+})
+
+Photo.virtual('thumbnail').get(function () {
+  return '/uploads/photos/640x640/' + this.filename
+})
 
 /**
  * STATICS
  */
 
 Photo.statics.createAndSaveToDisk = async function (fields, readable) {
-  const image = await Jimp.read(readable.path);
+  const image = await Jimp.read(readable.path)
   // const { width, height } = image.bitmap;
-  const photo = new this(fields);
-  photo.extension = image.getExtension();
+  const photo = new this(fields)
+  photo.extension = image.getExtension()
   image
-    // .scaleToFit(Math.min(width, 800), Math.min(height, 600))
-    .write(photo.path);
-  fs.unlink(readable.path); // probably don't need to use fs.unlink(). The OS will do the clean up.
-  await photo.save();
-  return photo;
-};
+    .write('public' + photo.url)
+    .cover(640, 640)
+    .write('public' + photo.thumbnail)
+  fs.unlink(readable.path) // probably don't need to use fs.unlink(). The OS will do the clean up.
+  await photo.save()
+  return photo
+}
 
 Photo.statics.removeFromDisk = async function (query) {
-  const photos = await this.find(query);
-  await Promise.all(photos.map(photo => photo.removeFromDisk()));
-};
+  const photos = await this.find(query)
+  await Promise.all(photos.map(photo => photo.removeFromDisk()))
+}
 
 Photo.statics.removeFromDiskAndDb = async function (query) {
-  await this.removeFromDisk(query);
-  await this.remove(query);
-};
+  await this.removeFromDisk(query)
+  await this.remove(query)
+}
 
 /**
  * METHODS
  */
 
 Photo.methods.removeFromDisk = async function () {
-  await fs.unlink(this.path);
-};
+  await fs.unlink('public' + this.url)
+  await fs.unlink('public' + this.thumbnail)
+}
 
 Photo.methods.removeFromDiskAndDb = async function () {
-  await this.removeFromDisk();
-  await this.remove();
-};
+  await this.removeFromDisk()
+  await this.remove()
+}
 
 Photo.methods.populateLikesCount = async function () {
-  this.likesCount = await User.find({ likes: { $in: [this.id] } }).count();
-};
+  this.likesCount = await User.find({ likes: { $in: [this.id] } }).count()
+}
 
 Photo.methods.isLiked = function (user) {
-  return !!user.likes.find(like => like.toString() === this.id);
-};
+  return !!user.likes.find(like => like.toString() === this.id)
+}
 
-module.exports = mongoose.model('Photo', Photo);
+module.exports = mongoose.model('Photo', Photo)
