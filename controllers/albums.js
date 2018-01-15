2e5dd06ac7c2b8fd605fbf84f5d6db7d2b456b0a
Sergei <you@example.com>
2018-01-15 21:50:03 +0200

    albums api

--- controllers/albums.js
+++ controllers/albums.js
@@ -3,55 +3,43 @@ const Album = require('../models/Album')
 const Photo = require('../models/Photo')
 
 exports.loadAlbumByid = async (id, ctx, next) => {
-  ctx.assert(mongoose.Types.ObjectId.isValid(id), 404, 'Invalid link!')
+  ctx.assert(mongoose.Types.ObjectId.isValid(id), 421, 'Not Found')
 
   ctx.album = await Album.findById(id)
-  ctx.assert(ctx.album, 404, 'Album not found!')
+
+  ctx.assert(ctx.album, 404, 'Not Found')
 
   await next()
 }
 
-exports.put = async (ctx, next) => {
-  if (!ctx.request.files.length) ctx.throw(400, 'Album cover is required!')
-  const { name, description } = ctx.request.body
-  const album = new Album({ name, description, author: ctx.state.user })
-  const cover = album.cover = await Photo.createAndSaveToDisk({ album, author: ctx.state.user }, ctx.request.files[0])
+exports.post = async (ctx) => {
+  const {name, description} = ctx.request.body
 
-  try {
-    await album.save()
-  } catch (err) {
-    cover.removeFromDiskAndDb()
-    throw err
-  }
-
-  ctx.redirect(`/albums/${album.id}`)
-}
-
-exports.get = async (ctx, next) => {
-  await ctx.album.populate('photos').populate('cover').execPopulate()
-  ctx.render('albums/show', {
-    album: ctx.album,
-    title: ctx.album.name
+  const album = await Album.create({
+    name,
+    description,
+    author: ctx.state.user
   })
+
+  ctx.body = album
 }
 
-exports.patch = async (ctx, next) => {
-  const { name, description } = ctx.request.body
+exports.get = async (ctx) => {
+  ctx.body = ctx.album
+}
 
-  Object.assign(ctx.album, { name, description })
-  if (ctx.request.files.length) {
-    ctx.album.cover = await Photo.createAndSaveToDisk({ album: ctx.album }, ctx.request.files[0])
-  }
+exports.patch = async (ctx) => {
+  const {name, description} = ctx.request.body
+
+  Object.assign(ctx.album, {name, description})
+
   await ctx.album.save()
 
-  ctx.flash('success', 'Album successfully updated')
-  ctx.redirect('back')
+  ctx.body = ctx.album
 }
 
-exports.delete = async (ctx, next) => {
-  await Photo.removeFromDiskAndDb({ album: ctx.album.id })
+exports.delete = async (ctx) => {
   await ctx.album.remove()
 
-  ctx.flash('success', 'Album successfully removed')
-  ctx.redirect('/')
+  ctx.body = 'OK'
 }
--- models/Album.js
+++ models/Album.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose')
-const Photo = require('../models/Photo')
+// const Photo = require('../models/Photo')
 
-const Album = new mongoose.Schema({
+const albumSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
   },
   description: {
     type: String,
-    default: 'Lorem ipsum'
   },
-  cover: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Photo',
-    required: true
-  },
+  // cover: {
+  //   type: mongoose.Schema.ObjectId,
+  //   ref: 'Photo',
+  //   required: true
+  // },
   author: {
     type: mongoose.Schema.ObjectId,
     ref: 'User',
@@ -25,14 +24,22 @@ const Album = new mongoose.Schema({
   timestamps: true
 })
 
-Album.virtual('photos', {
+albumSchema.virtual('photos', {
   ref: 'Photo',
   localField: '_id',
   foreignField: 'album'
 })
 
-Album.methods.populatePhotosCount = async function () {
-  this.photosCount = await Photo.find({album: {$in: [this.id]}}).count()
-};
+// Album.methods.populatePhotosCount = async function () {
+//   this.photosCount = await Photo.find({album: {$in: [this.id]}}).count()
+// }
 
-module.exports = mongoose.model('Album', Album)
+function autopopulate(next) {
+  this.populate('author')
+  next()
+}
+
+albumSchema.pre('find', autopopulate)
+albumSchema.pre('findOne', autopopulate)
+
+module.exports = mongoose.model('Album', albumSchema)
