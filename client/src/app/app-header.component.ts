939b530c10ef7f45aac457ed1bc0c87eb730fdc1
Sergei <you@example.com>
2018-02-13 23:22:35 +0200

    header styling

--- client/src/app/app-header.component.ts
+++ client/src/app/app-header.component.ts
@@ -4,21 +4,25 @@ import { User } from './auth/auth.service'
 @Component({
   selector: 'app-header',
   template: `
-    <header>
-      <mat-toolbar color="primary">
-        <mat-toolbar-row>
-          <a routerLink="/" mat-button>My Photos App {{ url$ | async }}</a>
-          <div class="spacer"></div>
+    <mat-toolbar color="primary" class="app-header mat-elevation-z4">
+      <mat-toolbar-row>
+        <a routerLink="/" mat-button>My Photos App {{ url$ | async }}</a>
+        <div class="spacer"></div>
 
-          <a routerLink="/" mat-button *ngIf="user">{{ user.email }}</a>
-          <button (click)="logout.emit()" mat-button *ngIf="user">Log out</button>
+        <a routerLink="/" mat-button *ngIf="user">{{ user.email }}</a>
+        <button (click)="logout.emit()" mat-button *ngIf="user">Log out</button>
 
-          <a routerLink="/auth/login" mat-button *ngIf="!user && url !== '/auth/login'">Log in</a>
-          <a routerLink="/auth/signup" mat-button *ngIf="!user && url !== '/auth/signup'">Sign up</a>
-        </mat-toolbar-row>
-      </mat-toolbar>
-    </header>
-  `
+        <a routerLink="/auth/login" mat-button *ngIf="!user && url !== '/auth/login'">Log in</a>
+        <a routerLink="/auth/signup" mat-button *ngIf="!user && url !== '/auth/signup'">Sign up</a>
+      </mat-toolbar-row>
+    </mat-toolbar>
+  `,
+  styles: [`
+    .app-header {
+      position: relative;
+      z-index: 10;
+    }
+  `]
 })
 
 export class AppHeaderComponent implements OnInit {
