0095092d6caf17d148c4d6d5531ab99520387301
Sergei <you@example.com>
2018-02-16 17:00:09 +0200

    add photos cmp upd

--- client/src/app/core/components/add-photos.component.ts
+++ client/src/app/core/components/add-photos.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core'
-import { MAT_DIALOG_DATA } from '@angular/material'
+import { Component, OnInit, Inject, Output, EventEmitter } from '@angular/core'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material'
 import { Photo } from '../photo.model'
 
 @Component({
@@ -7,27 +7,34 @@ import { Photo } from '../photo.model'
   template: `
     <h2 mat-dialog-title>Add photos</h2>
     <mat-dialog-content>
-      <div class="dropzone" [dropzone]="config" (success)="onUploadSuccess($event)"></div>
+      <div
+        class="dropzone"
+        [dropzone]="config"
+        (sending)="onSending($event)"
+        (success)="onUploadSuccess($event)"
+      ></div>
     </mat-dialog-content>
     <mat-dialog-actions>
-      <button mat-button mat-dialog-close>Close</button>
+      <button mat-button mat-dialog-close [disabled]="!!pendingRequests">Close</button>
     </mat-dialog-actions>
   `
 })
-
 export class AddPhotosComponent implements OnInit {
-  @ViewChild('dropzone') dropzone: ElementRef
+  pendingRequests = 0
 
   @Output() photoAdded = new EventEmitter<Photo>()
 
   constructor(
+    private dialogRef: MatDialogRef<AddPhotosComponent>,
     @Inject(MAT_DIALOG_DATA) private data: any
   ) { }
 
   get config() {
+    const { url, headers } = this.data
+
     return {
-      url: this.data.url,
-      headers: this.data.headers,
+      url,
+      headers,
       paramName: 'photo',
       acceptedFiles: 'image/*'
     }
@@ -35,7 +42,14 @@ export class AddPhotosComponent implements OnInit {
 
   ngOnInit() { }
 
+  onSending([file, request, data]: [File, XMLHttpRequest, FormData]) {
+    this.pendingRequests += 1
+    if (!this.dialogRef.disableClose) { this.dialogRef.disableClose = true }
+  }
+
   onUploadSuccess([file, response, event]: [File, Photo, ProgressEvent]) {
     this.photoAdded.emit(response)
+    this.pendingRequests -= 1
+    if (!this.pendingRequests) { this.dialogRef.disableClose = false }
   }
 }
